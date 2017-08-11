dbc52976735024348c466c1c148eb90d2204c7a9
David Henley <henleydr@gmail.com>
2017-08-10 21:28:38 -0500

    Conditional Header with Fake Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 class Header extends Component {
+  state = {
+    loggedIn: false
+  }
+
+  renderLogin() {
+    return (
+      <FlatButton onTouchTap={() => this.setState({ loggedIn: true })} label="Login" />
+    );
+  }
+
+  renderProfile() {
+    return (
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText="David Henley" />
+        <MenuItem onTouchTap={() => this.setState({ loggedIn: false })} primaryText="Sign Out" />
+      </IconMenu>
+    );
+  }
+
   render() {
     return (
       <AppBar
         title="My App"
         onLeftIconButtonTouchTap={this.props.toggleDrawer}
+        iconElementRight={this.state.loggedIn ? this.renderProfile() : this.renderLogin()}
       />
     );
   }
